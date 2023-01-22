52469ef54b808480c34629772b45c1b4fdedb233
Andreu Huguet <andreuhuguet@gmail.com>
2023-01-22 18:11:09 +0100

    Format links to dictionary

---
+++ formatLinks.js
@@ -0,0 +1,34 @@
+// import 'links.js'
+
+const getSaga = link => {
+    // https://multimedia.xarxacatala.cat/one-piece/saga-{saga}/op_cat-{cap_3nums}.mp4
+
+    const saga = link.split('/')
+        .filter(part => part.startsWith('saga-'))
+        [0]
+        .replace('saga-', '')
+
+    return parseInt(saga)
+}
+
+const getCap = link => {
+    // https://multimedia.xarxacatala.cat/one-piece/saga-{saga}/op_cat-{cap_3nums}.mp4
+
+    const cap = link.split('/')
+        .filter(part => part.startsWith('op_cat-'))
+        [0]
+        .replace('op_cat-', '')
+        .replace('.mp4', '')
+
+    return parseInt(cap)
+}
+
+let dict = {}
+const sagues = [...new Set(links.map(link => getSaga(link)))]
+sagues.forEach(saga => dict[saga] = [])
+
+links
+    .map(link => [getSaga(link), getCap(link)])
+    .forEach(([saga, cap]) => dict[saga].push(cap))
+
+console.log(dict)
