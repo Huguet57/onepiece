490fed060d9b364e5078921e399be70566a01dc4
Andreu Huguet <andreuhuguet@gmail.com>
2023-01-22 17:52:03 +0100

    Print list of existant URLs

--- get_urls.py
+++ get_urls.py
@@ -5,6 +5,19 @@ def exists(path):
     return r.status_code == requests.codes.ok
 
 def format(saga, cap):
-    return f'https://multimedia.xarxacatala.cat/one-piece/saga-{saga}/op_cat-{cap}.mp4'
+    cap_3nums = ''
+    if cap < 10: cap_3nums = '00' + str(cap)
+    elif cap < 100: cap_3nums = '0' + str(cap)
+    elif cap < 1000: cap_3nums = str(cap)
 
-print(exists(format(18, 155)))
+    return f'https://multimedia.xarxacatala.cat/one-piece/saga-{saga}/op_cat-{cap_3nums}.mp4'
+
+saga = 1
+cap = 1
+
+while cap < 1000:
+    cap_exists = exists(format(saga, cap))
+    if cap_exists: print(format(saga, cap))
+
+    if not cap_exists: saga += 1
+    else: cap += 1
